3ce6c39179c1b4543aeb3c576c01cfb178f3538a
Isaac Cidreira <isaaccidreira@hotmail.com>
2022-08-31 11:00:02 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import styles from './style.module.scss'
+import logoSvg from '../../../public/images/logo.svg'
+import { SignInButton } from '../SignInButton'
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src={logoSvg} alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  )
+}
