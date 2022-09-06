d2b8e599d95492bebe65f7e4b140bda3abf41745
Isaac Cidreira <isaaccidreira@hotmail.com>
2022-09-05 23:06:29 -0300

    projeto finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,14 +2,20 @@ import Image from 'next/image'
 import styles from './style.module.scss'
 import logoSvg from '../../../public/images/logo.svg'
 import { SignInButton } from '../SignInButton'
+import { ActiveLink } from '../ActiveLink'
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src={logoSvg} alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href='/'>
+            <a className={styles.active}>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href='/posts'>
+            <a>Posts</a>
+          </ActiveLink>
+
         </nav>
 
         <SignInButton />
